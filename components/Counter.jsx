1d37be9642e2b4cfa9449dd8de20c7d9fb045208
silqwer <silqwer@gmail.com>
2024-07-29 07:25:08 +0900

    feat: 미니 프로젝트 안내 및 카운터 앱 제작

---
+++ components/Counter.jsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useState } from "react";
+
+export default function Counter() {
+  const [counter, setCounter] = useState(0);
+
+  const increase = () => setCounter(counter + 1);
+  const decrease = () => setCounter(counter - 1);
+
+  return (
+    <div>
+      <p>{counter}</p>
+      <button type="button" onClick={increase}>
+        +
+      </button>
+      <button type="button" onClick={decrease}>
+        -
+      </button>
+    </div>
+  );
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,15 @@
-import Link from 'next/link';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import ProductList from '@/components/product-list/ProductList';
-import ProductHeader from '@/components/ProductHeader';
+import Counter from "@/components/Counter";
+import ProductList from "@/components/product-list/ProductList";
+import ProductHeader from "@/components/ProductHeader";
 
 // '/'에 해당하는 페이지 컴포넌트
 function ProductPage() {
-  const headerTitle = '상품 목록 페이지';
+  const headerTitle = "상품 목록 페이지";
 
   return (
     <div>
       <ProductHeader title={headerTitle} />
+      <Counter />
       <ProductList></ProductList>
     </div>
   );
