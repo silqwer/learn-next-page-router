207d6ae08b075f419703e7791386e6c706bd995f
silqwer <silqwer@gmail.com>
2024-07-30 07:38:36 +0900

    feat: cypress 공통 훅 처리

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
   extends: [
-    "plugin:cypress/recommended",
-    "next/core-web-vitals",
-    "plugin:prettier/recommended",
+    'plugin:cypress/recommended',
+    'next/core-web-vitals',
+    'plugin:prettier/recommended',
   ],
-  plugins: ["prettier", "cypress"],
+  plugins: ['prettier', 'cypress'],
   rules: {
     // 선언되지 않은 변수 또는 임포트 구문 정리 규칙
-    "no-undef": "error",
+    'no-undef': 'error',
   },
 };
--- cypress/e2e/counter.cy.js
+++ cypress/e2e/counter.cy.js
@@ -1,50 +1,50 @@
-describe("카운터 앱", () => {
+describe('카운터 앱', () => {
   beforeEach(() => {
-    cy.visit("http://localhost:3000/count");
+    cy.visit('http://localhost:3000/count');
   });
 
-  it("카운터 페이지에 방문한다", () => {});
+  it('카운터 페이지에 방문한다', () => {});
 
-  it("카운터 페이지에 방문하면 0이 표시된다", () => {
-    cy.get("[data-cy=counter]").should("have.text", "0");
+  it('카운터 페이지에 방문하면 0이 표시된다', () => {
+    cy.get('[data-cy=counter]').should('have.text', '0');
   });
 
-  it("카운터를 증가시킬 수 있다", () => {
-    cy.get("[data-cy=add-button]").contains("+").click();
-    cy.get("[data-cy=counter]").should("have.text", "1");
+  it('카운터를 증가시킬 수 있다', () => {
+    cy.get('[data-cy=add-button]').contains('+').click();
+    cy.get('[data-cy=counter]').should('have.text', '1');
   });
 
-  it("카운터를 감소시킬 수 있다", () => {
-    cy.get("[data-cy=add-button]").click();
-    cy.get("[data-cy=sub-button]").click();
-    cy.get("[data-cy=counter]").should("have.text", "0");
+  it('카운터를 감소시킬 수 있다', () => {
+    cy.get('[data-cy=add-button]').click();
+    cy.get('[data-cy=sub-button]').click();
+    cy.get('[data-cy=counter]').should('have.text', '0');
   });
 
-  it("카운터를 증가시키고 감소시킬 수 있다", () => {
-    cy.get("[data-cy=add-button]").click();
-    cy.get("[data-cy=add-button]").click();
-    cy.get("[data-cy=sub-button]").click();
-    cy.get("[data-cy=counter]").should("have.text", "1");
+  it('카운터를 증가시키고 감소시킬 수 있다', () => {
+    cy.get('[data-cy=add-button]').click();
+    cy.get('[data-cy=add-button]').click();
+    cy.get('[data-cy=sub-button]').click();
+    cy.get('[data-cy=counter]').should('have.text', '1');
   });
 
-  it("카운터를 감소시키고 증가시킬 수 있다", () => {
-    cy.get("[data-cy=sub-button]").click();
-    cy.get("[data-cy=sub-button]").click();
-    cy.get("[data-cy=add-button]").click();
-    cy.get("[data-cy=counter]").should("have.text", "-1");
+  it('카운터를 감소시키고 증가시킬 수 있다', () => {
+    cy.get('[data-cy=sub-button]').click();
+    cy.get('[data-cy=sub-button]').click();
+    cy.get('[data-cy=add-button]').click();
+    cy.get('[data-cy=counter]').should('have.text', '-1');
   });
 
-  it("카운터를 10번 증가시킬 수 있다", () => {
+  it('카운터를 10번 증가시킬 수 있다', () => {
     for (let i = 0; i < 10; i++) {
-      cy.get("[data-cy=add-button]").click();
+      cy.get('[data-cy=add-button]').click();
     }
-    cy.get("[data-cy=counter]").should("have.text", "10");
+    cy.get('[data-cy=counter]').should('have.text', '10');
   });
 
-  it("카운터를 10번 감소시킬 수 있다", () => {
+  it('카운터를 10번 감소시킬 수 있다', () => {
     for (let i = 0; i < 10; i++) {
-      cy.get("[data-cy=sub-button]").click();
+      cy.get('[data-cy=sub-button]').click();
     }
-    cy.get("[data-cy=counter]").should("have.text", "-10");
+    cy.get('[data-cy=counter]').should('have.text', '-10');
   });
 });
