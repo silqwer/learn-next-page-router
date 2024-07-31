b8be5439a42a2e794189ad92081201bfec5175aa
silqwer <silqwer@gmail.com>
2024-08-01 07:46:40 +0900

    feat: cypress baseUrl 설정

--- cypress/e2e/product-page.cy.js
+++ cypress/e2e/product-page.cy.js
@@ -1,6 +1,12 @@
 describe('상품 목록 페이지', () => {
   it('페이지에 진입하면 상품 목록이 표시된다. ', () => {
-    cy.visit('http://localhost:3000');
+    cy.visit('/');
     cy.getByCy('product-item').should('be.visible');
+  });
+
+  it('상품을 클릭하면 상품 상세 페이지로 이동한다.', () => {
+    cy.visit('/');
+    cy.getByCy('product-item').first().click();
+    cy.url().should('include', '/products/');
   });
 });
