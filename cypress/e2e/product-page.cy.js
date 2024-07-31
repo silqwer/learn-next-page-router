1008d39ed7884b5d0ddd229bc149adde7bd63c10
silqwer <silqwer@gmail.com>
2024-08-01 08:10:50 +0900

    feat: 테스트 코드의 적절한 assertion 개수

--- cypress/e2e/product-page.cy.js
+++ cypress/e2e/product-page.cy.js
@@ -1,12 +1,24 @@
 describe('상품 목록 페이지', () => {
-  it('페이지에 진입하면 상품 목록이 표시된다. ', () => {
+  beforeEach(() => {
+    // prepare - 사전작업
     cy.visit('/');
+  });
+
+  it('페이지에 진입하면 상품 목록이 표시된다. ', () => {
     cy.getByCy('product-item').should('be.visible');
   });
 
-  it('상품을 클릭하면 상품 상세 페이지로 이동한다.', () => {
-    cy.visit('/');
+  it('상품 목록에서 상품을 클릭하면 상품 상세 페이지로 이동한다.', () => {
     cy.getByCy('product-item').first().click();
     cy.url().should('include', '/products/');
+  });
+
+  it('네비게이션바의 장바구니 링크를 클릭하면 장바구니 페이지로 이동한다.', () => {
+    // action - 인터렉션
+    cy.getByCy('cart-link').click();
+
+    // assertion - 검증
+    cy.url().should('include', '/cart');
+    cy.getByCy('cart-header').should('be.visible');
   });
 });
--- layouts/Layout.jsx
+++ layouts/Layout.jsx
@@ -4,7 +4,13 @@ export default function Layout({ children }) {
   return (
     <div>
       <nav>
-        <Link href="/">상품 목록</Link> | <Link href="/cart">장바구니</Link>
+        <Link href="/" data-cy="product-link">
+          상품 목록
+        </Link>{' '}
+        |{' '}
+        <Link href="/cart" data-cy="cart-link">
+          장바구니
+        </Link>
       </nav>
       <div>{children}</div>
     </div>
